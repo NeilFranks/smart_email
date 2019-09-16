d51f776db33742348d90a73baf35bacbb2d2806d
NeilFranks <neilmichaelfranks@gmail.com>
2019-09-16 00:20:43 -0700

    dont need django_heroku here

--- datamanager/urls.py
+++ datamanager/urls.py
@@ -1,4 +1,3 @@
-import django_heroku
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
@@ -11,6 +10,3 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
