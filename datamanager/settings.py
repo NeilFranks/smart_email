5279e1f5cf0ede0d70551c97abbc94ac7e6fe7bc
NeilFranks <neilmichaelfranks@gmail.com>
2019-09-16 00:19:56 -0700

    fix settings

--- datamanager/settings.py
+++ datamanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,6 +120,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Change 'default' database configuration with $DATABASE_URL.
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 
 # Static files (CSS, JavaScript, Images)
