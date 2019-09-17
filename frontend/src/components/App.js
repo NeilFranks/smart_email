3e114524cc79192b6d8ac28ef7e011d671919c10
Neil <neilmichaelfranks@gmail.com>
2019-09-17 13:37:29 -0700

    base stuff for redux

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,25 @@
-import React, { Component, Fragment } from 'react';
-import ReactDom from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDom from "react-dom";
 
-import Header from './layout/Header';
-import Dashboard from './users/Dashboard';
+import Header from "./layout/Header";
+import Dashboard from "./users/Dashboard";
+
+import { Provider } from "react-redux";
+import store from "../store";
 
 class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Header />
-                <div className="container">
-                    <Dashboard />
-                </div>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </Fragment>
+      </Provider>
+    );
+  }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById("app"));
