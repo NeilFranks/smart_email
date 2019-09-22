cd5cbfeef64a9cf0a54b85a31aa0f60ee3a7019a
Neil <neilmichaelfranks@gmail.com>
2019-09-22 15:05:44 -0700

    do verification in meta tag instead

--- frontend/views.py
+++ frontend/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
 
 
 # @login_required(redirect_field_name="next", login_url=None)
 def home(request):
     return render(request, "frontend/home.html")
-
-
-def verify(request):
-    return render(request, "googlea247c86106d85d33.html")
