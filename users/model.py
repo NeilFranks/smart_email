307d28485a44a278ac29bcf6305dc4b20f15c014
Brad A Duszynski <bradduszynski@gmail.com>
2019-10-24 20:33:37 -0700

    Update model.py

--- users/model.py
+++ users/model.py
@@ -5,6 +5,7 @@ import email
 import re
 from django.core import serializers
 from sklearn.svm import LinearSVC
+from collections import Counter
 
 class Category(object):
 
@@ -58,8 +59,56 @@ def read_email(raw_email):
     
     return mail_dict
 
-def check_email(con, msg):
-    _, data = con.search(None, (""))
+def remove_common_words(word_frequency):
+    commonWordList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+                      "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
+                      "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom",
+                      "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being",
+                      "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but",
+                      "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about",
+                      "against", "between", "into", "through", "during", "before", "after", "above", "below", "to",
+                      "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then",
+                      "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few",
+                      "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+                      "too", "very", "can", "will", "just", "don", "should", "now", "nbsp", "enron" ]
+
+    words = list(word_frequency.keys())
+    for word in words:
+        if word in commonWordList:
+            del (word_frequency[word])
+        elif len(word) <= 2:
+            del (word_frequency[word])
+
+    return word_frequency
+
+def get_mcw_from_emails(list_of_emails):
+    list_of_body_words = []
+    for mail in list_of_emails:
+        body = mail["Body"].split()
+        if body:
+            list_of_body_words += body
+    
+    word_freq = Counter(list_of_body_words)
+    word_freq = remove_common_words(word_freq)
+    return word_freq.most_common(3000)
+
+def check_email(con, msg, category):
+    con.select(mailbox='"{}"'.format(category.categoryName))
+    email_list = list()
+    _, data = con.search(None, ("(ALL)"))
+    for mail in data[0].split():
+        _, raw_data = con.fetch(mail, '(RFC822)')
+        new_msg = read_email(raw_data[0][1])
+        email_list.append(new_msg)
+    # END OFF POINT
+    # TODO: get mcw from all emails
+    # Have train_labels [0:len(category_emails)] be 0 | [len(category_emails):len(all_excluding_category_emails)] be 1
+    # extract features into training matrix
+    # train linearSVC()
+    # Decide if proper in category
+    mcw = get_mcw_from_emails(email_list)
+    train_labels = np.zeros()
+    return True
 
 def scan_emails(categories):
     con = imaplib.IMAP4_SSL('imap.gmail.com')
@@ -75,7 +124,7 @@ def scan_emails(categories):
         local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
         for category in categories:
             if category.time < local_date:
-                valid_email = check_email(con, msg)
+                valid_email = check_email(con, msg, category)
                 if (valid_email):
                     uid = con.search(None, '(X-GM-MSGID) "%s"' % msgID)
                     uid = uid[1][0]
