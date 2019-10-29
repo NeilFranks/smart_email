6674e31db64cf8926da4b0ad6b250d10e6627f08
Neil <neilmichaelfranks@gmail.com>
2019-10-29 15:02:36 -0700

    additional component works, just says holo

---
+++ frontend/src/components/users/EmailList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export class EmailList extends Component {
+  render() {
+    return <h1> holo </h1>;
+  }
+}
+
+export default connect(
+  null,
+  null
+)(EmailList);
--- frontend/src/components/users/ViewEmailDashboard.js
+++ frontend/src/components/users/ViewEmailDashboard.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
+import EmailList from "./EmailList";
 
 export default function ViewEmailDashboard() {
   return (
     <Fragment>
       <h1>hey</h1>
+      <EmailList />
     </Fragment>
   );
 }
