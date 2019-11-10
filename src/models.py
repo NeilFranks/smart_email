1b4d0d30f33d641a0d9b243c1a8f70393f4d4c89
Neil <neilmichaelfranks@gmail.com>
2019-11-09 17:31:09 -0700

    category model

--- src/models.py
+++ src/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.TextField()
+    classifier = models.TextField()
+    time = models.DateTimeField()
+    owner = models.ForeignKey(
+        User, related_name="category", on_delete=models.CASCADE, null=True)
+
+
 class ConnectedEmail(models.Model):
     creds = models.TextField()
     address = models.TextField()
